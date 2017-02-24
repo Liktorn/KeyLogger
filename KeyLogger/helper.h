36d5547605440bda73aa78e548350bb0a2c70114
Winnerstam <brevbomb@hotmail.com>
2017-02-24 11:33:29 +0100

    JUst added helper.h

---
+++ KeyLogger/helper.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <ctime>
+#include <string>
+#include <sstream> //converting various types to strings
+
+//creating our own namespace
+namespace Helper
+{
+	template <class T>
+
+	std::string ToString(const T &)
+
+		//stopped looking at lecture 8 7 min in
+}
